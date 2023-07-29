a888431970ea99fcd57a65b1dd12937b6cf48992
Marlom Silva <125768418+Marlomss@users.noreply.github.com>
2023-07-29 01:54:54 -0300

    :white_check_mark:
    Atualização de links

--- js/posts.js
+++ js/posts.js
@@ -1,109 +1,33 @@
 var posts = [
   {
-    "imagem": "https://picsum.photos/id/100/348/185",
-    "titulo": "Um título",
-    "autor": "José Otavio",
-    "data": "02 Fev 2019",
-    "imagem_post": "https://picsum.photos/id/100/1110/590",
-    "texto": "1 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a convallis tellus. Maecenas vel velit nec urna auctor iaculis vitae ut neque. Integer mollis eros vestibulum lacus accumsan, non vestibulum odio hendrerit. Sed volutpat sem vitae libero commodo, aliquet convallis tellus viverra. Nulla molestie faucibus erat sit amet semper. In hac habitasse platea dictumst. Donec eget neque non elit lobortis efficitur. Nunc vitae neque sollicitudin enim faucibus mattis ut ut tellus."
+    "imagem": "https://www.reciclasampa.com.br/imgs/conteudos/07_tenha_paciencia_caminhao_coleta_padrao.jpg",
+    "titulo": "Coleta Domiciliar Seletiva",
+    "orgao": "Governo de São Paulo",
+    "url_post": "https://www.prefeitura.sp.gov.br/cidade/secretarias/spregula/residuos_solidos/coleta_seletiva/index.php?p=4623",
   },
   {
-    "imagem": "https://picsum.photos/id/200/348/185",
-    "titulo": "Um título 2",
-    "autor": "José Otavio",
-    "data": "02 Fev 2019",
-    "imagem_post": "https://picsum.photos/id/200/1110/590",
-    "texto": "2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a convallis tellus. Maecenas vel velit nec urna auctor iaculis vitae ut neque. Integer mollis eros vestibulum lacus accumsan, non vestibulum odio hendrerit. Sed volutpat sem vitae libero commodo, aliquet convallis tellus viverra. Nulla molestie faucibus erat sit amet semper. In hac habitasse platea dictumst. Donec eget neque non elit lobortis efficitur. Nunc vitae neque sollicitudin enim faucibus mattis ut ut tellus.",
-    "video": "https://www.youtube.com/embed/y__ero7suPo"
+    "imagem": "http://www.prefeitura.sp.gov.br/cidade/secretarias/upload/pev.JPG",
+    "titulo": "Pontos de Entrega Voluntária (PEV's)",
+    "orgao": "Governo de São Paulo",
+    "url_post": "https://www.prefeitura.sp.gov.br/cidade/secretarias/spregula/residuos_solidos/coleta_seletiva/?p=43479",
   },
   {
-    "imagem": "https://picsum.photos/id/300/348/185",
-    "titulo": "Um título 3",
-    "autor": "José Otavio",
-    "data": "02 Fev 2019",
-    "imagem_post": "https://picsum.photos/id/300/1110/590",
-    "texto": "3 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a convallis tellus. Maecenas vel velit nec urna auctor iaculis vitae ut neque. Integer mollis eros vestibulum lacus accumsan, non vestibulum odio hendrerit. Sed volutpat sem vitae libero commodo, aliquet convallis tellus viverra. Nulla molestie faucibus erat sit amet semper. In hac habitasse platea dictumst. Donec eget neque non elit lobortis efficitur. Nunc vitae neque sollicitudin enim faucibus mattis ut ut tellus.",
-    "video": "https://www.youtube.com/embed/vvt2hgREqag"
+    "imagem": "https://www.prefeitura.sp.gov.br/cidade/secretarias/upload/subpreituras/WhatsApp%20Image%202023-04-25%20at%2014_14_22.jpeg",
+    "titulo": "ECOPONTO - Estação de Entrega Voluntária de Inservíveis",
+    "orgao": "Governo de São Paulo",
+    "url_post": "https://www.prefeitura.sp.gov.br/cidade/secretarias/spregula/residuos_solidos/ecopontos/index.php?p=4626",
   },
   {
-    "imagem": "https://picsum.photos/id/400/348/185",
-    "titulo": "Um título 4",
-    "autor": "José Otavio",
-    "data": "02 Fev 2019",
-    "imagem_post": "https://picsum.photos/id/400/1110/590",
-    "texto": "4 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a convallis tellus. Maecenas vel velit nec urna auctor iaculis vitae ut neque. Integer mollis eros vestibulum lacus accumsan, non vestibulum odio hendrerit. Sed volutpat sem vitae libero commodo, aliquet convallis tellus viverra. Nulla molestie faucibus erat sit amet semper. In hac habitasse platea dictumst. Donec eget neque non elit lobortis efficitur. Nunc vitae neque sollicitudin enim faucibus mattis ut ut tellus.",
-    "video": "https://www.youtube.com/embed/yIO_7K4-JjU"
+    "imagem": "https://www.prefeitura.sp.gov.br/cidade/secretarias/upload/ciclo_coleta_domiciliar(2).jpg",
+    "titulo": "Coleta Domiciliar Comum",
+    "orgao": "Governo de São Paulo",
+    "url_post": "https://www.prefeitura.sp.gov.br/cidade/secretarias/spregula/residuos_solidos/residuos_solidos/domiciliar/index.php?p=4636",
   },
   {
-    "imagem": "https://picsum.photos/id/55/348/185",
-    "titulo": "Um título 5",
-    "autor": "José Otavio",
+    "imagem": "https://www.artereciclada.com.br/wp-content/uploads/2013/06/sabao-oleo-de-cozinha.jpg",
+    "titulo": "Faça sabão com óleo de cozinha usado (EXPERIÊNCIA + dica doméstica)",
+    "orgao": "You Tube",
     "data": "02 Fev 2019",
-    "imagem_post": "https://picsum.photos/id/55/1110/590",
-    "texto": "5 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a convallis tellus. Maecenas vel velit nec urna auctor iaculis vitae ut neque. Integer mollis eros vestibulum lacus accumsan, non vestibulum odio hendrerit. Sed volutpat sem vitae libero commodo, aliquet convallis tellus viverra. Nulla molestie faucibus erat sit amet semper. In hac habitasse platea dictumst. Donec eget neque non elit lobortis efficitur. Nunc vitae neque sollicitudin enim faucibus mattis ut ut tellus.",
-    "video": "https://www.youtube.com/embed/yIO_7K4-JjU"
-  },
-  {
-    "imagem": "https://picsum.photos/id/99/348/185",
-    "titulo": "Um título 5",
-    "autor": "José Otavio",
-    "data": "02 Fev 2019",
-    "imagem_post": "https://picsum.photos/id/99/1110/590",
-    "texto": "6 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a convallis tellus. Maecenas vel velit nec urna auctor iaculis vitae ut neque. Integer mollis eros vestibulum lacus accumsan, non vestibulum odio hendrerit. Sed volutpat sem vitae libero commodo, aliquet convallis tellus viverra. Nulla molestie faucibus erat sit amet semper. In hac habitasse platea dictumst. Donec eget neque non elit lobortis efficitur. Nunc vitae neque sollicitudin enim faucibus mattis ut ut tellus.",
-    "video": "https://www.youtube.com/embed/yIO_7K4-JjU"
-  },
-  {
-    "imagem": "https://picsum.photos/id/73/348/185",
-    "titulo": "Um título 5",
-    "autor": "José Otavio",
-    "data": "02 Fev 2019",
-    "imagem_post": "https://picsum.photos/id/73/1110/590",
-    "texto": "7 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a convallis tellus. Maecenas vel velit nec urna auctor iaculis vitae ut neque. Integer mollis eros vestibulum lacus accumsan, non vestibulum odio hendrerit. Sed volutpat sem vitae libero commodo, aliquet convallis tellus viverra. Nulla molestie faucibus erat sit amet semper. In hac habitasse platea dictumst. Donec eget neque non elit lobortis efficitur. Nunc vitae neque sollicitudin enim faucibus mattis ut ut tellus.",
-    "video": "https://www.youtube.com/embed/yIO_7K4-JjU"
-  },
-  {
-    "imagem": "https://picsum.photos/id/85/348/185",
-    "titulo": "Um título 5",
-    "autor": "José Otavio",
-    "data": "02 Fev 2019",
-    "imagem_post": "https://picsum.photos/id/85/1110/590",
-    "texto": "8 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a convallis tellus. Maecenas vel velit nec urna auctor iaculis vitae ut neque. Integer mollis eros vestibulum lacus accumsan, non vestibulum odio hendrerit. Sed volutpat sem vitae libero commodo, aliquet convallis tellus viverra. Nulla molestie faucibus erat sit amet semper. In hac habitasse platea dictumst. Donec eget neque non elit lobortis efficitur. Nunc vitae neque sollicitudin enim faucibus mattis ut ut tellus.",
-    "video": "https://www.youtube.com/embed/yIO_7K4-JjU"
-  },
-  {
-    "imagem": "https://picsum.photos/id/91/348/185",
-    "titulo": "Um título 5",
-    "autor": "José Otavio",
-    "data": "02 Fev 2019",
-    "imagem_post": "https://picsum.photos/id/91/1110/590",
-    "texto": "9 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a convallis tellus. Maecenas vel velit nec urna auctor iaculis vitae ut neque. Integer mollis eros vestibulum lacus accumsan, non vestibulum odio hendrerit. Sed volutpat sem vitae libero commodo, aliquet convallis tellus viverra. Nulla molestie faucibus erat sit amet semper. In hac habitasse platea dictumst. Donec eget neque non elit lobortis efficitur. Nunc vitae neque sollicitudin enim faucibus mattis ut ut tellus.",
-    "video": "https://www.youtube.com/embed/yIO_7K4-JjU"
-  },
-  {
-    "imagem": "https://picsum.photos/id/100/348/185",
-    "titulo": "Um título 5",
-    "autor": "José Otavio",
-    "data": "02 Fev 2019",
-    "imagem_post": "https://picsum.photos/id/103/1110/590",
-    "texto": "10 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a convallis tellus. Maecenas vel velit nec urna auctor iaculis vitae ut neque. Integer mollis eros vestibulum lacus accumsan, non vestibulum odio hendrerit. Sed volutpat sem vitae libero commodo, aliquet convallis tellus viverra. Nulla molestie faucibus erat sit amet semper. In hac habitasse platea dictumst. Donec eget neque non elit lobortis efficitur. Nunc vitae neque sollicitudin enim faucibus mattis ut ut tellus.",
-    "video": "https://www.youtube.com/embed/yIO_7K4-JjU"
-  },
-  {
-    "imagem": "https://picsum.photos/id/111/348/185",
-    "titulo": "Um título 5",
-    "autor": "José Otavio",
-    "data": "02 Fev 2019",
-    "imagem_post": "https://picsum.photos/id/111/1110/590",
-    "texto": "11 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a convallis tellus. Maecenas vel velit nec urna auctor iaculis vitae ut neque. Integer mollis eros vestibulum lacus accumsan, non vestibulum odio hendrerit. Sed volutpat sem vitae libero commodo, aliquet convallis tellus viverra. Nulla molestie faucibus erat sit amet semper. In hac habitasse platea dictumst. Donec eget neque non elit lobortis efficitur. Nunc vitae neque sollicitudin enim faucibus mattis ut ut tellus.",
-    "video": "https://www.youtube.com/embed/yIO_7K4-JjU"
-  },
-  {
-    "imagem": "https://picsum.photos/id/12/348/185",
-    "titulo": "Um título 5",
-    "autor": "José Otavio",
-    "data": "02 Fev 2019",
-    "imagem_post": "https://picsum.photos/id/12/1110/590",
-    "texto": "12 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a convallis tellus. Maecenas vel velit nec urna auctor iaculis vitae ut neque. Integer mollis eros vestibulum lacus accumsan, non vestibulum odio hendrerit. Sed volutpat sem vitae libero commodo, aliquet convallis tellus viverra. Nulla molestie faucibus erat sit amet semper. In hac habitasse platea dictumst. Donec eget neque non elit lobortis efficitur. Nunc vitae neque sollicitudin enim faucibus mattis ut ut tellus.",
-    "video": "https://www.youtube.com/embed/yIO_7K4-JjU"
+    "url_post": "https://www.youtube.com/watch?v=FsV4gAfIPdM",
   }
 ];
